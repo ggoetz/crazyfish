15e0c649e33851312c7f23fdbfe1fbc109c9a676
bowenpan <bowenpan@gsb.stanford.edu>
2014-02-13 15:31:23 -0800

    Latest peekadate

---
+++ peekadate/config.py
@@ -0,0 +1,4 @@
+PAGES = {'test2.html': ('test.html', {'work': 'Google', 'school': 'Stanford'})}
+
+FLOWS = {'test2.html': {'orange-flat-button': ('click', 'test.html'), '.nephritis-flat-button': ('click', 'test.html')}}
+	
